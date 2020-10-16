58dccc4eeab4a661514f1e61f9cedd7bbb5e17a6
Martin Jhon More Panta <mmorepan@everis.com>
2020-10-16 04:09:09 -0500

    Work In Progress - Savepoint

--- src/com/moreci/bean/ParameterBean.groovy
+++ src/com/moreci/bean/ParameterBean.groovy
@@ -22,8 +22,8 @@ class ParameterBean {
     def customWorkspace = workspace ?: this.workspace
     def suffixPath = this.getWorkspaceSuffixPath()
     customWorkspace += (suffixPath) ? "/${suffixPath}" : ""
-    echo "${customWorkspace.toString()}"
-    echo "${suffixPath.toString()}"
+    println "${customWorkspace.toString()}"
+    println "${suffixPath.toString()}"
     return customWorkspace
   }
 
