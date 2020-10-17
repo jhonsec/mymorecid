dfae9069ff28e8d4ace8f0fb70c092270266db9e
Martin Jhon More Panta <mmorepan@everis.com>
2020-10-17 06:11:11 -0500

    Work In Progress - Savepoint

--- src/com/moreci/steps/SonarScannerStep.groovy
+++ src/com/moreci/steps/SonarScannerStep.groovy
@@ -7,7 +7,7 @@ class SonarScannerStep extends StepAbstract {
   @Override
   void run() {
     root.dir(this.source) {
-      root.withSonarQubeEnv ('SonarQube MoreCI') {
+      root.withSonarQubeEnv ('SonarQube_MoreCI') {
         root.sh "${this.command}"
       }
       root.timeout (time: 10, unit: "MINUTES") {
@@ -15,7 +15,7 @@ class SonarScannerStep extends StepAbstract {
       }
     }
     //root.hygieiaSonarPublishStep ceQueryIntervalInSeconds: '10', ceQueryMaxAttempts: '30'
-    root.hygieiaCodeQualityPublishStep jacocoFilePattern: './target/jacoco.exec'
-    root.hygieiaCodeQualityPublishStep jacocoFilePattern: './target/site/jacoco/jacoco.xml'
+//    root.hygieiaCodeQualityPublishStep jacocoFilePattern: './target/jacoco.exec'
+//    root.hygieiaCodeQualityPublishStep jacocoFilePattern: './target/site/jacoco/jacoco.xml'
   }
 }
